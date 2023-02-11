d5b1adfec1c00b068d24a442dc7b93f037cc241b
LaurenMcQuillan <115508658+LaurenMcQuillan@users.noreply.github.com>
2023-02-11 15:12:25 -0500

    Part 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -16,3 +16,12 @@ suspension_coil_df <- read.csv(file='Suspension_Coil.csv', check.names=F, string
 total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 # Create lot_summary dataframe
 lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+
+########################################################### PART 3 ###########################################################
+# t test across all manufacturing lots
+t.test(suspension_coil_df$PSI, mu=1500)
+# t test for individual manufacturing lots
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot1")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot2")$PSI, mu = 1500)
+t.test(subset(suspension_coil_df,Manufacturing_Lot=="Lot3")$PSI, mu = 1500)
