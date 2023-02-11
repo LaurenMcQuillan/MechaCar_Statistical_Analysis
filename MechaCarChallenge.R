2d75bf268cb5c08810c7909f7247cdfd861050d4
LaurenMcQuillan <115508658+LaurenMcQuillan@users.noreply.github.com>
2023-02-11 14:58:47 -0500

    Part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,8 +2,17 @@
 # Load dplyr
 library(dplyr)
 # Import & read .csv 
-MechaCar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
+mechacar_mpg_df <- read.csv(file='MechaCar_mpg.csv',check.names = F,stringsAsFactors = F)
 # Perform linear regression
-lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_df)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df)
 # Determine p-value and r-squared value
-summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg_df)) 
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mechacar_mpg_df)) 
+
+
+########################################################### PART 2 ###########################################################
+# Import & read .csv 
+suspension_coil_df <- read.csv(file='Suspension_Coil.csv', check.names=F, stringsAsFactors=F)
+# Create total_summary dataframe
+total_summary <- suspension_coil_df %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+# Create lot_summary dataframe
+lot_summary <- suspension_coil_df %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
